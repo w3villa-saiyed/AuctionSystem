ab409da9616144d43ef64347c94501f899c43d15
adnan <w3villa-saiyed@gmail.com>
2022-10-20 12:26:06 +0530

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,29 @@
 class HomeController < ApplicationController
-  before_action :role_change
+  # before_action :role_change
 
   def index
     @categories = Category.all
   end
 
-   def role_change
-    if current_user&.has_role?('admin')
-      redirect_to admin_adminview_path
-    elsif current_user&.has_role?('seller')
-      # redirect_to '/'
-    end
-    if current_user&.has_role?('bidder')
-      # redirect_to '/'
-    end
+
+  def bidder
+
   end
+
+  def seller
+
+  end
+
+
+
+  #  def role_change
+  #   if current_user&.has_role?('admin')
+  #     redirect_to root_path
+  #   elsif current_user&.has_role?('seller')
+  #     # redirect_to '/'
+  #   end
+  #   if current_user&.has_role?('bidder')
+  #     # redirect_to '/'
+  #   end
+  # end
 end
