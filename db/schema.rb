c9665047cd546abb5bd80b4cc25d0c198edf5ca9
adnan <w3villa-saiyed@gmail.com>
2022-10-20 12:45:16 +0530

    erroe

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_091201) do
     t.string "unconfirmed_email"
     t.string "phone"
     t.string "otp"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
